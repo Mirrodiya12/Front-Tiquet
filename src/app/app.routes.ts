853bd6746011ffffe7823de7bd2b77b4629d9398
SkDanielP <113185316+SkDanielP@users.noreply.github.com>
2025-05-25 19:55:35 -0500

    se corrigen los errores de carpeta y se listan los eventos despues de ingresar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ListarEventosComponent } from './listar-eventos/listar-eventos.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'eventos', component: ListarEventosComponent },
   { path: '**', redirectTo: '/login' }
 ];
